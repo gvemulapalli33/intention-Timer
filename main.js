ee2d5d3a01dfc22711fbf397cd328767306494a2
gvemulapalli33 <gvemulapalli33@gmail.com>
2020-11-02 20:52:28 -0800

    add current activity view

--- main.js
+++ main.js
@@ -6,11 +6,14 @@ class ActivityTimer {
 
     constructor() {
         this.activity = null;
-        this.$task = document.querySelector('.task');
+        this.$task = document.querySelector('.task-input');
         this.$min = document.querySelector('.minutes');
         this.$sec = document.querySelector('.seconds');
+        this.$errTask = document.querySelector('.task-err');
         this.addEventListeners();
         this.currentCategory = '';
+        this.$currentActivityDetails = document.querySelector('.current-activity');
+        this.$activityDetails = document.querySelector('.activity');
     }
 
     addEventListeners() {
@@ -26,12 +29,13 @@ class ActivityTimer {
 
     changeActivityStyle(event) {
         let {target} = event;
-        let input = target.tagName === 'input' ||  target.closest('input');
-        if (input.getAttribute('name') && input.getAttribute('name') === 'category') {
+        let input =  target.tagName === 'input' ||  target.closest('input');
+        if (input && input.getAttribute('name') && input.getAttribute('name') === 'category') {
             let {checked, value} = input;
             let label = checked && input.closest('label');
             this.clearSelectedBorders();
             label.classList.add(`${value}-selected`);
+            this.currentCategory = value;
         }
     }
 
@@ -47,14 +51,13 @@ class ActivityTimer {
 
     validateInput(event) {
         let {target} = event;
-        if (target.classList.contains('task')) {
-            let errorMsg = document.querySelector('.task-err');
+        if (target.classList.contains('task-input')) {
             let {value} = target;
             target.classList.remove('error');
-            errorMsg.innerText = '';
+            this.$errTask.innerText = '';
             if (value.length < 5) {
                 target.classList.add('error');
-                errorMsg.innerText = `length of input should be more than 5 characters`;
+                this.$errTask.innerText = `length of input should be more than 5 characters`;
             }   
         }
         if (target.classList.contains('minutes') || target.classList.contains('seconds')) {
@@ -79,11 +82,54 @@ class ActivityTimer {
         }
     }
 
+    validateForm() {
+        let task = this.$task.value;
+        let min = this.$min.value;
+        let sec = this.$sec.value;
+        if( !task || !min || !sec) {
+            this.showError();
+        }
+    }
+
+    showError() {
+        this.$errTask.innerHTML = `<span class="error-txt"><span class="warning"></span> A description is required.</span>`;
+    }
+
     getActivityData() {
         let task = this.$task.value;
-        let minutes = this.$min.value;
-        let seconds = this.$sec.value;
-        this.activity = new Activity(this.currentCategory, task, minutes, seconds, false, Activity.counter++)
+        let minutes = this.formatTime(this.$min.value);
+        let seconds = this.formatTime(this.$sec.value);
+        this.activity = new Activity(this.currentCategory, task, minutes, seconds, false, Activity.counter++);
+        this.startActivity();
+    }
+
+    formatTime(time) {
+        console.log(time);
+        if(+time < 10) {
+            return "0" + time;
+        } else {
+            return time;
+        }
+    }
+
+    startActivity() {
+       this.$activityDetails.hidden = true;
+       this.$currentActivityDetails.hidden = false;
+       let className = `bg-category ${this.currentCategory}-selected`;
+       this.$currentActivityDetails.innerHTML = `
+       <h2 class="sub-title">Current Activity</h2>
+       <section class="activity-details">
+            <h3>${this.activity.description}</h3>
+            <h1 class="clock">
+                <span class="timer">${this.activity.minutes}</span>
+                <span class="seperator">:</span>
+                <span class="timer">${this.activity.seconds}</span>
+            </h1>
+            <article class="${className}">
+                <p class="status">start</p>
+            </article>
+        </section>    
+       `;
     }
 
 }
